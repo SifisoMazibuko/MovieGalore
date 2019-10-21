4f28bcb2c71ff4744b7f02d943bf426655258c42
sifisomazibuko <sifisomazibuko@hotmail.com>
2019-10-21 19:07:23 +0200

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MovieDetailComponent,
+    NavbarComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
